ef14a509d66f9ec2d3d5b27d3a4080c1f6e77d42
Evan Irving-Pease <ekirving@hotmail.com>
2017-07-20 18:13:35 -0400

    minor update

--- build_database.py
+++ build_database.py
@@ -5,11 +5,12 @@ from db_conn import db_conn
 
 from populate_qtls import populate_qtls
 from populate_samples import populate_samples
-from populate_coverage import populate_coverage
+from populate_coverage import *
 from discover_snps import discover_snps
 
 # populate_qtls()
 # populate_samples()
+# populate_intervals()
 populate_coverage()
 
 discover_snps('sample_reads_q20', min_baseq=20, min_mapq=20, min_dist=3, max_qtl=100000)

2508cb83e8d6f505275d526998c84f9d77cc5aca
Evan Irving-Pease <ekirving@hotmail.com>
2017-08-21 14:44:00 +0100

    added notes files

---
+++ alleletraj-commands.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# mysqldump w/ gzip
+mysqldump -u root -p --single-transaction allele_trajectory \
+	| gzip > allele_trajectory-fulldump.sql.gz
+
+# import w/ gzip
+gunzip < allele_trajectory-fulldump.sql.gz | mysql -u root -p 

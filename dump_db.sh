4dafdb7e137ca88324b17cef67ef1250820ee67d
Evan Irving-Pease <ekirving@hotmail.com>
2018-03-16 10:53:15 +0000

    tweak to db backup script

--- dump_db.sh
+++ dump_db.sh
@@ -3,4 +3,7 @@
 now=`date '+%Y%m%d-%H%M'`;
 filename="allele_trajectory-$now.sql.gz"
 
-mysqldump allele_trajectory --ignore-table=allele_trajectory.dbsnp_complete | gzip > $filename
+mysqldump allele_trajectory \
+    --ignore-table=allele_trajectory.dbsnp_complete \
+    --ignore-table=allele_trajectory.modern_snps \
+    | gzip > $filename

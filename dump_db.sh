6af8df0c60dd776dafea14c86557ad8826202978
Evan Irving-Pease <ekirving@hotmail.com>
2018-03-23 13:49:06 +0000

    tweak to dump tables

--- dump_db.sh
+++ dump_db.sh
@@ -5,5 +5,4 @@ filename="allele_trajectory-$now.sql.gz"
 
 mysqldump allele_trajectory \
     --ignore-table=allele_trajectory.dbsnp_complete \
-    --ignore-table=allele_trajectory.modern_snps \
     | gzip > $filename

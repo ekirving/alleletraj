676ae8ec6dc4202ffdb3028260650b8d902c0043
Evan Irving-Pease <ekirving@hotmail.com>
2018-04-09 11:55:05 +0100

    bin all samples into one master spreadsheet

--- graph_bins.py
+++ graph_bins.py
@@ -20,63 +20,65 @@ BIN_PERCENT = 0.5  # samples must overlap a bin by >= 50%
 # open a db connection
 dbc = db_conn()
 
-for bin_lower in range(BIN_START, BIN_END, - BIN_WIDTH):
-    bin_upper = bin_lower - BIN_WIDTH + 1
+with open("tsv/all-bins.tsv", "wb") as tsv_file:
+    fields = ['bin', 'accession', 'map_prcnt', 'status', 'age', 'confident', 'lower', 'upper', 'width', 'overlap',
+              'perct_overlap']
 
-    # get all the samples which overlap this bin by >= BIN_OVERLAP
-    samples = dbc.get_records_sql("""
-         SELECT *,
-                @width := lower - upper AS width,
-                @overlap := LEAST(lower, {binlower}) - GREATEST(upper, {binupper}) AS overlap,
-                @overlap / @width AS perct_overlap
-           FROM (
-                  # get the most precise dates for each sample, and the normalised wild/dom status
-                  SELECT s.accession,
-                         s.map_prcnt,
-                         CASE
-                             WHEN s.status LIKE '%domestic%' THEN 'Domestic'
-                             WHEN s.status LIKE '%wild%'     THEN 'Wild'
-                             ELSE 'NA'
-                         END AS status,
-                         s.age,
-                         COALESCE(c14.confident, sd.confident) confident,
-                         COALESCE(c14.lower, sd.lower, sd.median + {uncert}) lower,
-                         COALESCE(c14.upper, sd.upper, sd.median - {uncert}) upper
-                    FROM samples s
-                    JOIN sample_dates sd
-                      ON s.age <=> sd.age
-               LEFT JOIN sample_dates_c14 c14
-                      ON c14.accession = s.accession
-                   WHERE s.species = '{species}'
-                     AND s.valid = 1
+    writer = csv.DictWriter(tsv_file, fieldnames=fields, delimiter='\t')
+    writer.writeheader()
 
-                ) as age
+    for bin_lower in range(BIN_START, BIN_END, - BIN_WIDTH):
+        bin_upper = bin_lower - BIN_WIDTH + 1
 
-          WHERE lower >= {binupper}
-            AND upper <= {binlower}
-         HAVING overlap/width >= {binpercent}""".format(species=species,
-                                                        uncert=MEDIAN_AGE_UNCERT,
-                                                        binpercent=BIN_PERCENT,
-                                                        binlower=bin_lower,
-                                                        binupper=bin_upper), key=None)
+        # get all the samples which overlap this bin by >= BIN_OVERLAP
+        samples = dbc.get_records_sql("""
+             SELECT *,
+                    @width := lower - upper AS width,
+                    @overlap := LEAST(lower, {binlower}) - GREATEST(upper, {binupper}) AS overlap,
+                    @overlap / @width AS perct_overlap
+               FROM (
+                      # get the most precise dates for each sample, and the normalised wild/dom status
+                      SELECT s.accession,
+                             s.map_prcnt,
+                             CASE
+                                 WHEN COALESCE(s.gmm_status, s.status) LIKE '%wild%'     THEN 'Wild'
+                                 WHEN COALESCE(s.gmm_status, s.status) LIKE '%domestic%' THEN 'Domestic'
+                                 ELSE 'NA'
+                             END AS status,
+                             s.age,
+                             COALESCE(c14.confident, sd.confident) confident,
+                             COALESCE(c14.lower, sd.lower, sd.median + {uncert}) lower,
+                             COALESCE(c14.upper, sd.upper, sd.median - {uncert}) upper
+                        FROM samples s
+                        JOIN sample_dates sd
+                          ON s.age <=> sd.age
+                   LEFT JOIN sample_dates_c14 c14
+                          ON c14.accession = s.accession
+                       WHERE s.species = '{species}'
+                         AND s.valid = 1
+    
+                    ) as age
+    
+              WHERE lower >= {binupper}
+                AND upper <= {binlower}
+             HAVING overlap/width >= {binpercent}""".format(species=species,
+                                                            uncert=MEDIAN_AGE_UNCERT,
+                                                            binpercent=BIN_PERCENT,
+                                                            binlower=bin_lower,
+                                                            binupper=bin_upper), key=None)
 
-    stub = "sample_bin-{lower:05d}-{upper:05d}".format(lower=bin_lower, upper=bin_upper)
-    label = "{lower:,} - {upper:,} BP".format(lower=bin_lower, upper=bin_upper)
+        stub = "{lower:05d}-{upper:05d}".format(lower=bin_lower, upper=bin_upper)
+        label = "{lower:,} - {upper:,} BP".format(lower=bin_lower, upper=bin_upper)
 
-    if not samples:
-        print "WARNING: No samples for bin {label}".format(label=label)
-        continue
+        if not samples:
+            print "WARNING: No samples for bin {label}".format(label=label)
+            continue
 
-    # write the data to disk
-    with open("tsv/{stub}.tsv".format(stub=stub), "wb") as tsv_file:
-        fields = ['accession', 'map_prcnt', 'status', 'age', 'confident', 'lower', 'upper', 'width', 'overlap',
-                  'perct_overlap']
-
-        writer = csv.DictWriter(tsv_file, fieldnames=fields, delimiter='\t')
-        writer.writeheader()
+        # write the data to disk
         for sample in samples:
+            sample['bin'] = stub
             writer.writerow(sample)
 
-    # now generate the plot
-    run_cmd(['Rscript', 'rscript/plot-bin-mapping.R', stub, label])
+        # now generate the plot
+        # run_cmd(['Rscript', 'rscript/plot-bin-mapping.R', stub, label])
 

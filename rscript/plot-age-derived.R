f00b2c9e0f543527a18dc80f0630ac00ab024054
Evan Irving-Pease <ekirving@hotmail.com>
2018-04-10 18:49:15 +0100

    added sample age plot histogram

--- rscript/plot-age-derived.R
+++ rscript/plot-age-derived.R
@@ -19,14 +19,18 @@ dat <- read.table(paste0("tsv/", stub, ".tsv"), header=TRUE, sep ='\t') #, strin
 dat <- na.omit(dat)
 
 # transversions only (i.e. no transitions [A <-> G] and [C <-> T]
-# dat <- dat[!(dat$ancestral %in% c('A', 'G') & dat$derived %in% c('A', 'G')),]
-# dat <- dat[!(dat$ancestral %in% c('C', 'T') & dat$derived %in% c('C', 'T')),]
+dat <- dat[!(dat$ancestral %in% c('A', 'G') & dat$derived %in% c('A', 'G')),]
+dat <- dat[!(dat$ancestral %in% c('C', 'T') & dat$derived %in% c('C', 'T')),]
+
+# load the sample median ages
+age <- read.table("tsv/sample-median-age.tsv", header=TRUE, sep ='\t')
 
 pdf(file=paste0("pdf/", stub, ".pdf"), width = 8, height = 7)
 
-ggplot(dat, aes(x = oldest_derived, y = oldest_sample, fill=trait)) +
+ggplot(dat) +
     geom_abline(color='grey') +
-    geom_point(size=2, shape=21) +
+    geom_histogram(data=age,binwidth = 500, aes(x = median, weight = 130), alpha = 0.3) +
+    geom_point(size=2, shape=21, aes(x = oldest_derived, y = oldest_sample, fill=trait)) +
     theme(
         plot.title = element_text(hjust = 0.5),
         legend.key = element_blank(),
@@ -36,7 +40,8 @@ ggplot(dat, aes(x = oldest_derived, y = oldest_sample, fill=trait)) +
     ggtitle(label) +
     scale_fill_discrete(name="QTL Trait Class") +
     scale_x_continuous(limits = c(0, 13000), breaks = seq(0, 13000, by = 1000)) +
-    scale_y_continuous(limits = c(0, 13000), breaks = seq(0, 13000, by = 1000)) +
+    scale_y_continuous(limits = c(0, 13000), breaks = seq(0, 13000, by = 1000),
+                       sec.axis = sec_axis(~./130, name = "Sample count")) +
     xlab("Earliest derived allele (BP)") +
     ylab("Earliest sample (BP)")
 

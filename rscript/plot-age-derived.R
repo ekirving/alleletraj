2ec60e21151a52dd0c238213a6ab2ad53b731872
Evan Irving-Pease <ekirving@hotmail.com>
2018-04-11 13:10:44 +0100

    change histogram to count of SNPs

--- rscript/plot-age-derived.R
+++ rscript/plot-age-derived.R
@@ -23,13 +23,21 @@ dat <- na.omit(dat)
 # dat <- dat[!(dat$ancestral %in% c('C', 'T') & dat$derived %in% c('C', 'T')),]
 
 # load the sample median ages
-age <- read.table("tsv/sample-median-age.tsv", header=TRUE, sep ='\t')
+age <- read.table("tsv/all-snps-counts.tsv", header=TRUE, sep ='\t')
+
+# count the number of SNPs covered in 100 year bins
+age.hist <- hist(age$median, breaks = seq(0, 13000, by=100), plot=FALSE)
+
+# log10 transform the result, as high coverage samples have millions of SNPs
+# age.log10 <- data.frame(age.hist$mids, log10(age.hist$counts)*500)
+# colnames(age.log10) <- c('mid', 'log10cnt')
 
 pdf(file=paste0("pdf/", stub, ".pdf"), width = 8, height = 7)
 
 ggplot(dat) +
     geom_abline(color='grey') +
-    geom_histogram(data=age,binwidth = 250, aes(x = median, weight = 130), alpha = 0.3) +
+    geom_histogram(data=age,binwidth = 100, aes(x = median, weight = 1/200), alpha = 0.3) +
+    # geom_bar(stat="identity", data=age.log10, aes(x = mid, y = log10cnt)) +
     geom_point(size=2, shape=21, aes(x = oldest_derived, y = oldest_sample, fill=trait)) +
     theme(
         plot.title = element_text(hjust = 0.5),
@@ -41,7 +49,8 @@ ggplot(dat) +
     scale_fill_discrete(name="QTL Trait Class") +
     scale_x_continuous(limits = c(0, 13150), breaks = seq(0, 13000, by = 1000)) +
     scale_y_continuous(limits = c(0, 13150), breaks = seq(0, 13000, by = 1000),
-                       sec.axis = sec_axis(~./130, name = "Sample count")) +
+                       sec.axis = sec_axis(trans=~.*200, name = "SNP count",
+                                           breaks = seq(0, 3e6, by = 5e5))) +
     xlab("Earliest derived allele (BP)") +
     ylab("Earliest sample (BP)")
 

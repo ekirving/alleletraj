1c21cea0c0801579404bfdd1f11e4215ada18ab1
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-08 13:35:33 +0100

    bug fix

--- ascertainment.py
+++ ascertainment.py
@@ -232,7 +232,7 @@ def fetch_neutral_snps():
     for chrom, perct in autosomes.iteritems():
 
         # get the weighted number of SNPs for this chrom
-        num_snps = int(round(NUM_NEUTRAL_SNPS/perct))
+        num_snps = int(round(NUM_NEUTRAL_SNPS * perct))
 
         dbc.execute_sql("""
             INSERT
@@ -281,7 +281,7 @@ def fetch_ancestral_snps():
     for chrom, perct in autosomes.iteritems():
 
         # get the weighted number of SNPs for this chrom
-        num_snps = int(round(NUM_ANCESTRAL_SNPS / perct))
+        num_snps = int(round(NUM_ANCESTRAL_SNPS * perct))
 
         dbc.execute_sql("""
             INSERT
@@ -316,21 +316,21 @@ def perform_ascertainment():
 
     print("INFO: Starting ascertainment process")
 
-    # clear any existing SNPs
-    dbc = db_conn()
-    dbc.execute_sql("TRUNCATE TABLE ascertainment")
-
-    # fetch all the GWAS peaks from the QTL database
-    fetch_gwas_peaks()
-
-    # fetch the best flanking SNPs for each GWAS peak
-    fetch_gwas_flanking_snps()
-
-    # fetch the best SNPs from the selective sweep loci
-    fetch_selective_sweep_snps()
-
-    # get all MC1R snps
-    fetch_mc1r_snps()
+    # # clear any existing SNPs
+    # dbc = db_conn()
+    # dbc.execute_sql("TRUNCATE TABLE ascertainment")
+    #
+    # # fetch all the GWAS peaks from the QTL database
+    # fetch_gwas_peaks()
+    #
+    # # fetch the best flanking SNPs for each GWAS peak
+    # fetch_gwas_flanking_snps()
+    #
+    # # fetch the best SNPs from the selective sweep loci
+    # fetch_selective_sweep_snps()
+    #
+    # # get all MC1R snps
+    # fetch_mc1r_snps()
 
     # get neutral SNPs (excluding all QTLs and gene regions, w/ buffer)
     fetch_neutral_snps()

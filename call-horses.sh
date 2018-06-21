b3d9e2df2a7f7593c8a543c9bbf8b3f885cba9ad
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 10:53:10 +0100

    permissions tweak

--- call-horses.sh
+++ call-horses.sh
@@ -3,9 +3,9 @@
 # call DOM / Przewalski horses
 bcftools mpileup --fasta-ref fasta/Sus_scrofa.Sscrofa10.2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt \
 	| bcftools call --multiallelic-caller --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file modern_DOM.vcf &
+	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/modern_DOM.vcf &
 
 # call DOM2 / Domestic horses
 bcftools mpileup --fasta-ref fasta/Sus_scrofa.Sscrofa10.2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt \
 	| bcftools call --multiallelic-caller --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file modern_DOM2.vcf &
+	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/modern_DOM2.vcf &

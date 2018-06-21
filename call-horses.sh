c6dd076e69c645758e3f677b573e9356bc182a6e
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 15:12:51 +0100

    improved horse variant calling

--- call-horses.sh
+++ call-horses.sh
@@ -3,7 +3,7 @@
 # call DOM / Przewalski horses
 cmd1=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions {} \
-	| bcftools call --multiallelic-caller --output-type v \
+	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM.samples --output-type v \
 	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
 EOF
 )
@@ -13,7 +13,7 @@ parallel ${cmd1} ::: {1..31} X
 # call DOM2 / Domestic horses
 cmd2=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions {} \
-	| bcftools call --multiallelic-caller --output-type v \
+	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM2.samples --output-type v \
 	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
 EOF
 )

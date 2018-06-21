c6eb13e3b72ee0750714073b06e079cfca592a20
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 16:00:51 +0100

    bug fix

--- call-horses.sh
+++ call-horses.sh
@@ -4,7 +4,7 @@
 cmd1=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions chr{} \
 	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM.samples --output-type v \
-	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
+	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf
 EOF
 )
 
@@ -14,7 +14,7 @@ parallel ${cmd1} ::: {1..31} X
 cmd2=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions chr{} \
 	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM2.samples --output-type v \
-	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
+	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf
 EOF
 )
 

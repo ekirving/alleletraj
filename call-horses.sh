c89259b60f734682b9a6de814436c5371638faa6
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 14:04:41 +0100

    tweaks

--- call-horses.sh
+++ call-horses.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
 # call DOM / Przewalski horses
-cmd=$(cat <<EOF
+cmd1=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions {} \
 	| bcftools call --multiallelic-caller --output-type v \
 	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
 EOF
 )
 
-parallel ${cmd} ::: {1..31} X
+parallel ${cmd1} ::: {1..31} X
 
 # call DOM2 / Domestic horses
-cmd=$(cat <<EOF
+cmd2=$(cat <<EOF
 bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions {} \
 	| bcftools call --multiallelic-caller --output-type v \
 	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
 EOF
 )
+
+parallel ${cmd2} ::: {1..31} X
 
 echo "FINISHED calling modern horses!!"

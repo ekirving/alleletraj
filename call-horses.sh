3e6e83668bebbf2b0db588473f517bf04500d25b
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 15:59:33 +0100

    bug fix for calling SNPs in modern horses

--- call-horses.sh
+++ call-horses.sh
@@ -2,9 +2,9 @@
 
 # call DOM / Przewalski horses
 cmd1=$(cat <<EOF
-bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions {} \
+bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions chr{} \
 	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM.samples --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
+	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
 EOF
 )
 
@@ -12,9 +12,9 @@ parallel ${cmd1} ::: {1..31} X
 
 # call DOM2 / Domestic horses
 cmd2=$(cat <<EOF
-bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions {} \
+bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions chr{} \
 	| bcftools call --multiallelic-caller --ploidy-file ./data/horses.ploidy --samples-file ./data/horses_DOM2.samples --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
+	| bcftools view --types snps --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
 EOF
 )
 

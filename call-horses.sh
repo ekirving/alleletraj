f2e30cd64d2e74b1014eec2d5957ab72cd4450ec
Evan Irving-Pease <ekirving@hotmail.com>
2018-06-21 14:00:18 +0100

    parallelised the VCF calling

--- call-horses.sh
+++ call-horses.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 # call DOM / Przewalski horses
-bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt \
+cmd=$(cat <<EOF
+bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM.txt --regions {} \
 	| bcftools call --multiallelic-caller --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/modern_DOM.vcf &
+	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM_chr{}.vcf &
+EOF
+)
+
+parallel ${cmd} ::: {1..31} X
 
 # call DOM2 / Domestic horses
-bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt \
+cmd=$(cat <<EOF
+bcftools mpileup --fasta-ref fasta/Equus_caballus.EquCab2.dna.toplevel.fa --bam-list ./data/bam_files_horse_DOM2.txt --regions {} \
 	| bcftools call --multiallelic-caller --output-type v \
-	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/modern_DOM2.vcf &
-
-wait
+	| bcftools view --exclude-types indels,bnd,other --exclude INFO/INDEL=1 --output-file ./vcf/horse_DOM2_chr{}.vcf &
+EOF
+)
 
 echo "FINISHED calling modern horses!!"

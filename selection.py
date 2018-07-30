16b77efb785693d5a15ec60b4db4a34a95e86ab5
Evan Irving-Pease <ekirving@hotmail.com>
2018-07-30 18:16:49 +0100

    better error handling

--- selection.py
+++ selection.py
@@ -209,4 +209,4 @@ def model_selection(args):
 
     except Exception:
         # Put all exception text into an exception and raise that
-        raise Exception("".join(traceback.format_exception(*sys.exc_info())))
+        print(traceback.format_exception(*sys.exc_info()), file=sys.stderr)

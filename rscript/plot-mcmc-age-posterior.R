ea515a7daa739b3e8dc6d7227f6b8f9e51002bf1
Evan Irving-Pease <ekirving@hotmail.com>
2018-08-21 11:58:22 +0200

    tidy up for age density plot

--- rscript/plot-mcmc-age-posterior.R
+++ rscript/plot-mcmc-age-posterior.R
@@ -4,6 +4,9 @@ library(ggplot2)
 
 setwd('/Users/Evan/Dropbox/Code/alleletraj')
 
+# TODO command line params
+pop_size <- 16000
+gen_time <- 8
 burnin <- 1000 # i.e. 20%
 
 # load all the param files
@@ -14,10 +17,6 @@ names(mcmc.params) <- suppressMessages(names(read_tsv(files[1], col_names = T, n
 # convert diffusion units into calendar years
 mcmc.params$ageyrs <- mcmc.params$age * 2 * pop_size * gen_time
 
-# TODO command line params
-pop_size <- 16000
-gen_time <- 8
-
 dom1.age <- -5500
 dom2.age <- -4000
 
@@ -27,10 +26,10 @@ brk_width <- 5000
 ggplot(mcmc.params, aes(ageyrs)) +
 
     # show a density plot of the calander ages
-    geom_density(fill="darkgrey") +
+    geom_density(fill="#48a4e2", colour="#0d4869") +
 
     # plot the ages of the main domestication events
-    geom_vline(xintercept=c(dom1.age, dom2.age), linetype = "dashed", colour = 'red') +
+    geom_vline(xintercept=c(dom1.age, dom2.age), linetype = "dashed", colour = '#c94904') +
 
     # set the breaks for the x-axis
     scale_x_continuous(limits = c(max_age - 5000, 0),
@@ -49,4 +48,8 @@ ggplot(mcmc.params, aes(ageyrs)) +
     ylab("Density") +
 
     # tweak the theme
-    theme_minimal(base_size = 10)
+    theme_minimal(base_size = 10) +
+    theme(
+        # remove the vertical grid lines
+        panel.grid.major.x = element_blank()
+    )

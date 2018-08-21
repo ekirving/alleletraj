56e7363319301d27b311d10041502b4c7d54b02e
Evan Irving-Pease <ekirving@hotmail.com>
2018-08-21 11:46:51 +0200

    improvements to 2d density plots

--- rscript/plot-mcmc-density_2d.R
+++ rscript/plot-mcmc-density_2d.R
@@ -4,6 +4,10 @@ library(ggplot2)
 
 setwd('/Users/Evan/Dropbox/Code/alleletraj')
 
+
+# TODO command line params
+pop_size <- 16000
+gen_time <- 8
 burnin <- 1000 # i.e. 20%
 
 # load all the param files
@@ -18,29 +22,34 @@ mcmc.params$firstyrs <- mcmc.params$first_nonzero * 2 * pop_size * gen_time
 # convert end_freq back into an actual frequency
 mcmc.params$freq <- (1-cos(mcmc.params$end_freq))/2
 
-# TODO command line params
-pop_size <- 16000
-gen_time <- 8
-
 max_age <- -50000
 brk_width <- 5000
 
 ggplot(mcmc.params) +
 
-    # show a histogram
-    # geom_density_2d(aes(ageyrs, alpha1)) +
-    geom_density_2d(aes(ageyrs, freq)) +
+    # plot 2D density of Age vs Alpha
+    stat_density_2d(aes(x=ageyrs, y=alpha1, fill=..level..), geom="polygon",
+                    colour="white", show.legend=F) +
+
+    # # plot 2D density of plot Age vs Freq
+    # stat_density_2d(aes(x=ageyrs, y=freq, fill=..level..), geom="polygon",
+    #                 colour="white", show.legend=F) +
 
     scale_x_continuous(limits = c(max_age - 5000, 0),
                        breaks = seq(max_age, 0, by = brk_width),
                        labels = seq(max_age, 0, by = brk_width)/1000,
-                       minor_breaks = NULL,
-                       expand = c(0.01, 0))
+                       minor_breaks = NULL) +
 
+    # using limits() drops all data points that are not within the specified range,
+    # causing discontinuity of the density plot, while coord_cartesian() zooms
+    # without losing the data points.
+    coord_cartesian(xlim = c(max_age, 0)) +
 
-ggplot(mcmc.params) +
-    geom_density_2d(aes(alpha1, freq))
+    # label the plot and the axes
+    xlab("kyr BP") +
+    # ylab("Frequency") +
+    ylab("Alpha") +
 
+    # tweak the theme
+    theme_minimal(base_size = 10)
 
-ggplot(mcmc.params) +
-    geom_contour(aes(alpha1, freq), stat="density2d", bins = 30)

030606e3044d18d9b86e21989b696b1fae977f6e
Evan Irving-Pease <ekirving@hotmail.com>
2019-05-03 13:32:13 +0100

    added README and LICENSE

---
+++ alleletraj/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='alleletraj',
+      version='1.0',
+      description='Selection trajectories of genetic variants underlying domestic animal traits',
+      url='https://github.com/ekirving/alleletraj',
+      author='Evan K. Irving-Pease',
+      author_email='evan.irvingpease@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            # TODO add these
+      ],
+      include_package_data=True,
+      zip_safe=False)

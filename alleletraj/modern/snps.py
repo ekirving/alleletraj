975aa36ab7cf60737d5b57844aca4cb992718370
Evan Irving-Pease <evan.irvingpease@gmail.com>
2019-05-07 15:33:00 +0100

    removed old special casing for pigs

--- alleletraj/modern/snps.py
+++ alleletraj/modern/snps.py
@@ -14,35 +14,6 @@ from alleletraj import utils
 from alleletraj.db.load import CreateDatabase
 from vcf import BiallelicSNPsVCF
 
-# TODO move into spreadsheet
-FASTA_PATH = '/media/jbod2/raid1-sdc1/laurent/full_run_results/Pig/modern/FASTA'
-
-# extended FASTA codes for biallelic sites
-FASTA_MAP = {
-    'R': ['A', 'G'],
-    'Y': ['C', 'T'],
-    'K': ['G', 'T'],
-    'M': ['A', 'C'],
-    'S': ['C', 'G'],
-    'W': ['A', 'T'],
-}
-
-
-def decode_fasta(pileup):
-    """
-    Return list of haploid genotype calls, filtering out N values
-    """
-    return sum([FASTA_MAP.get(val, [val, val]) for val in pileup if val != 'N'], [])
-
-
-def stream_fasta(fin):
-    """
-    Convert a file input into an iterable which returns a single character, filtering out newline characters
-    """
-    for character in itertools.chain.from_iterable(fin):
-        if character != '\n':
-            yield character
-
 
 def mutation_type(alleles):
     """
@@ -51,140 +22,9 @@ def mutation_type(alleles):
     return 'ts' if set(alleles) == {'A', 'G'} or set(alleles) == {'C', 'T'} else 'tv'
 
 
-class ExternalFASTA(utils.PipelineExternalTask):
+class LoadModernSNPs(utils.PipelineTask):
     """
-    External task dependency for a chromosome FASTA file.
-
-    N.B. These have been created outside the workflow of this pipeline.
-
-    :type sample: str
-    :type chrom: str
-    """
-    sample = luigi.Parameter()
-    chrom = luigi.Parameter()
-
-    def output(self):
-        return luigi.LocalTarget('{}/{}/{}.fa'.format(FASTA_PATH, self.sample, self.chrom))
-
-
-class ModernSNPsFromFASTA(utils.PipelineTask):
-    """
-    Ascertain moderns SNPS, and estimate their allele frequency, from the given chromosome FASTA files.
-
-    :type species: str
-    :type chrom: str
-    """
-    species = luigi.Parameter()
-    chrom = luigi.Parameter()
-
-    def requires(self):
-        yield CreateDatabase(self.species)
-
-        # include the outgroup in the sample list, as we need it for polarization
-        for pop in self.all_populations:
-            for sample in self.all_populations[pop]:
-                yield ExternalFASTA(sample)  # TODO outgroup is no longer the first sample in the list
-
-    def output(self):
-        return luigi.LocalTarget('data/db/{}-{}.log'.format(self.basename, self.classname))
-
-    def run(self):
-        # get all the input fasta files
-        fasta_files = [fasta_file.path for fasta_file in self.input()[1:]]
-
-        # open a private connection to the db
-        dbc = self.db_conn()
-
-        with self.output().open('w') as fout:
-            fout.write('STARTED: Parsing {} fasta files.\n'.format(len(fasta_files)))
-
-            site = 0
-            num_snps = 0
-            data = []
-
-            for fasta in fasta_files:
-                # load the fasta data
-                fin = open(fasta, 'rU')
-
-                # discard header row
-                fin.readline()
-
-                # wrap the file in an iterator
-                data.append(stream_fasta(fin))
-
-                fout.write('LOADED: {}\n'.format(fasta))
-
-            # zip the sequences together so we can iterate over them one site at a time
-            for pileup in itertools.izip_longest(*data, fillvalue='N'):
-
-                # increment the counter
-                site += 1
-
-                # convert iterator into a list
-                pileup = list(pileup)
-
-                # get the outgroup alleles
-                out_allele = pileup.pop(0)
-
-                # decode the fasta format
-                haploids = decode_fasta(pileup)
-
-                # how many alleles are there at this site
-                num_alleles = len(set(haploids))
-
-                # we only want biallelic SNPs
-                if num_alleles > 2:
-                    fout.write('WARNING: Polyallelic site chr{}:{} = {}\n'.format(self.chrom, site, set(haploids)))
-                    continue
-
-                # count the haploid observations
-                observations = Counter(haploids)
-
-                # decode the outgroup allele
-                ancestral = set(decode_fasta(out_allele))
-
-                # we cannot handle variable sites in the outgroup
-                if len(ancestral) != 1:
-                    fout.write('WARNING: Unknown ancestral allele chr{}:{} = {}\n'.format(self.chrom, site, out_allele))
-                    continue
-
-                ancestral = ancestral.pop()
-                alleles = observations.keys()
-
-                if ancestral not in alleles:
-                    fout.write('WARNING: Polyallelic site chr{}:{} = {}, ancestral {}\n'
-                               .format(self.chrom, site, set(haploids), ancestral))
-                    continue
-
-                # is this mutation a transition or a transversion
-                snp_type = mutation_type(alleles)
-
-                alleles.remove(ancestral)
-                derived = alleles.pop()
-
-                # calculate the derived allele frequency (DAF)
-                daf = float(observations[derived]) / (observations[ancestral] + observations[derived])
-
-                record = {
-                    'chrom': self.chrom,
-                    'site': site,
-                    'ancestral': ancestral,
-                    'ancestral_count': observations[ancestral],
-                    'derived': derived,
-                    'derived_count': observations[derived],
-                    'type': snp_type,
-                    'daf': daf,  # TODO one per population
-                }
-
-                dbc.save_record('modern_snps', record)
-                num_snps += 1
-
-            fout.write('FINISHED: Added {:,} SNPs\n'.format(num_snps))
-
-
-class ModernSNPsFromVCF(utils.PipelineTask):
-    """
-    Ascertain moderns SNPS, and estimate their allele frequency, from the given chromosome VCF file.
+    Ascertain moderns SNPs and estimate their allele frequency from a chromosome VCF file.
 
     :type species: str
     :type chrom: str
@@ -263,25 +103,6 @@ class ModernSNPsFromVCF(utils.PipelineTask):
 
         with self.output().open('w') as fout:
             fout.write('Added {:,} SNPs'.format(num_snps))
-
-
-class LoadModernSNPs(utils.PipelineWrapperTask):
-    """
-    Ascertain modern SNPs in whole-genome data.
-
-    :type species: str
-    :type chrom: str
-    """
-    species = luigi.Parameter()
-    chrom = luigi.Parameter()
-
-    def requires(self):
-        if self.species == 'pig':
-            # special case of FASTA data for pig ascertainment
-            yield ModernSNPsFromFASTA(self.species, self.chrom)
-        else:
-            # parse the VCF files for all other species
-            yield ModernSNPsFromVCF(self.species, self.chrom)
 
 
 class ModernSNPsPipeline(utils.PipelineWrapperTask):
